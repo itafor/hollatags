c2f931934ad64a8cc77cb5401464bb56dd34bd96
francis <itaforfrancis@gmail.com>
2020-07-29 11:32:07 -0700

    api route updated

--- holla_tags_billing_api/routes/api.php
+++ holla_tags_billing_api/routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+
+    'prefix' => 'bill'
+
+], function () {
+
+    Route::post('users', 'BillingController@billUsers')->name('user.bill');
+
+});
